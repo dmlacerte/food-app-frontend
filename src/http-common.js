2e83425a7d6ca499e21f018d9d9e1d7b3774ac20
dmlacerte <dmlacerte@gmail.com>
2022-07-03 14:44:58 -0400

    dev branch

--- src/http-common.js
+++ src/http-common.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 export default axios.create({
-  baseURL: "https://immense-brushlands-47117.herokuapp.com/api",
-  // baseURL: "http://localhost:8080/api",
+  // baseURL: "https://immense-brushlands-47117.herokuapp.com/api",
+  baseURL: "http://localhost:8080/api",
   headers: {
     "Content-type": "application/json"
   }
